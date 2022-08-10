0208406fd6ebb74fcb6e206f69fb501040413b82
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-10 22:06:02 +0200

    refactor: add `root` and `home` pages

---
+++ src/root.tsx
@@ -0,0 +1,63 @@
+// @refresh reload
+import "@fontsource/poppins/latin-300.css";
+import "@fontsource/poppins/latin-400.css";
+import "@fontsource/poppins/latin-500.css";
+import "virtual:windi.css";
+
+import {
+  Html,
+  Head,
+  Meta,
+  Link,
+  Body,
+  Routes,
+  Scripts,
+  FileRoutes,
+  ErrorBoundary
+} from "solid-start";
+
+export default function () {
+  return (
+    <Html lang="en">
+      <Head>
+        <Meta charset="utf-8" />
+        <Meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+
+        <Link rel="icon" href="/favicon.ico" sizes="any" />
+        <Link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
+        <Meta name="theme-color" content="#17AA67" />
+
+        <Show when={import.meta.env.PROD}>
+          <Link rel="manifest" href="/manifest.webmanifest" />
+        </Show>
+
+        <Title>{APP_NAME}</Title>
+        <Meta name="title" content={APP_NAME} />
+        <Meta name="description" content={APP_DESCRIPTION} />
+        <Link rel="canonical" href={APP_URL} />
+
+        <Meta property="og:type" content="website" />
+        <Meta property="og:url" content={APP_URL} />
+        <Meta property="og:title" content={APP_NAME} />
+        <Meta property="og:description" content={APP_DESCRIPTION} />
+        {/* <Meta property="og:image" content="/banner.png" /> */}
+
+        <Meta property="twitter:card" content="summary_large_image" />
+        <Meta property="twitter:url" content={APP_URL} />
+        <Meta property="twitter:title" content={APP_NAME} />
+        <Meta property="twitter:description" content={APP_DESCRIPTION} />
+        {/* <Meta property="twitter:image" content="/banner.png" /> */}
+      </Head>
+      <Body>
+        <Suspense>
+          <ErrorBoundary>
+            <Routes>
+              <FileRoutes />
+            </Routes>
+          </ErrorBoundary>
+        </Suspense>
+        <Scripts />
+      </Body>
+    </Html>
+  );
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,78 @@
+export default () => {
+  const accounts = [] as any[] | null; // TODO
+
+  return (
+    <div class="h-screen w-screen bg-brand-primary dark:bg-brand-dark text-brand-white">
+      <header class="fixed top-0 w-full py-4 flex flex-col items-center justify-start">
+        <h1 class="font-bold text-3xl dark:text-brand-primary">{APP_NAME}</h1>
+        <p class="text-lg text-brand-light mb-4">Client Pronote non-officiel.</p>
+
+        <button>
+          Sombre/Clair{/* {theme === "light" ? "Mode Sombre" : "Mode Clair"} */}
+        </button>
+      </header>
+
+      <section class="h-full w-full flex items-center justify-center py-32 px-4">
+        <Show
+          when={accounts !== null && accounts}
+          fallback={
+            <p>Chargement des comptes...</p>
+          }
+        >
+          { accounts => (
+            <Show
+              when={accounts.length > 0}
+              fallback={
+                <div class="
+                  flex flex-col justify-center items-center gap-4 max-w-md p-6 rounded-lg
+  
+                  dark:bg-brand-primary dark:bg-opacity-20 dark:border-2 dark:border-brand-primary
+                  bg-brand-dark bg-opacity-20 border-2 border-brand-dark
+                ">
+                  <p class="text-sm sm:text-base opacity-100 text-center">
+                    Aucun compte sauvegardé localement !
+                  </p>
+                  <Link href="/login">
+                    Ajouter un compte Pronote
+                  </Link>
+                  </div>
+              }
+            >
+              <For each={accounts}>
+                {account => (
+                  <Link href={`/app/${account.slug}/dashboard`}>
+                  <div
+                    class="
+                      bg-brand-white rounded-xl text-brand-primary
+                      p-4 cursor-pointer hover:bg-opacity-80 transition-colors
+                      hover:shadow-sm
+                    "
+                  >
+                    <h2 class="font-semibold">
+                      {account.data.userInformations.ressource.L} ({account.data.userInformations.ressource.classeDEleve.L})
+                    </h2>
+                    <p class="text-opacity-60">
+                      {account.data.schoolInformations.General.NomEtablissement}
+                    </p>
+                  </div>
+                </Link>
+                )}
+              </For>
+            </Show>
+          )}
+        </Show>
+      </section>
+
+      <footer class="w-full fixed bottom-0 flex flex-col items-center justify-center h-16">
+        <a
+          class="font-medium text-brand-light dark:text-brand-white hover:text-opacity-60 transition-colors"
+          href="https://github.com/Vexcited/pornote"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  )
+}
