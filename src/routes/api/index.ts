66027d7162fe013bbbc17ad8c936b20e3d0fa298
Mikkel RINGAUD <mikkel@milescode.dev>
2022-09-21 18:20:30 +0000

    refactor: homepage api route

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -2,6 +2,10 @@ import { json } from "solid-start/server";
 
 export const get = () => {
   return json({
-    success: true
+    success: true,
+    data: {
+      github: "https://github.com/Vexcited/pornote",
+      api_documentation: null
+    }
   });
 };
