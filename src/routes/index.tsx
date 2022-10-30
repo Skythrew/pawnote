a958cfb10acc1f55ed8530ea6a330f4d45656ee7
Mikkel RINGAUD <mikkel@milescode.dev>
2022-10-30 21:44:22 +0100

    fix: home page interface

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -34,17 +34,17 @@ const RootHomePage: Component = () => {
   });
 
   return (
-    <div class="h-screen w-screen bg-brand-primary dark:bg-brand-dark text-brand-white">
-      <header class="fixed top-0 w-full py-4 flex flex-col items-center justify-start">
+    <div class="min-h-screen py-4 px-6 flex flex-col justify-between bg-brand-primary dark:bg-brand-dark text-brand-white">
+      <header class="w-full flex flex-col items-center justify-start">
         <h1 class="font-bold text-3xl dark:text-brand-primary">{APP_NAME}</h1>
         <p class="text-lg text-brand-light mb-4">Client Pronote non-officiel.</p>
 
-        <button onClick={settings.toggleGlobalThemeMode}>
+        <button class="shadow-lg rounded-md px-2 py-1 bg-brand-dark dark:bg-brand-primary" onClick={settings.toggleGlobalThemeMode}>
           {settings.globalThemeMode() === "dark" ? "Mode Clair" : "Mode Sombre"}
         </button>
       </header>
 
-      <section class="h-full w-full flex items-center justify-center py-32 px-4">
+      <section class="w-full flex flex-col gap-4 items-center justify-center px-4">
         <Show keyed
           when={availableSessions()}
           fallback={
@@ -55,54 +55,42 @@ const RootHomePage: Component = () => {
             <Show
               when={sessions.length > 0}
               fallback={
-                <div class="
-                  flex flex-col justify-center items-center gap-4 max-w-md p-6 rounded-lg
-
-                  dark:bg-brand-primary dark:bg-opacity-20 dark:border-2 dark:border-brand-primary
-                  bg-brand-dark bg-opacity-20 border-2 border-brand-dark
-                ">
-                  <p class="text-sm sm:text-base opacity-100 text-center">
-                    Aucun compte sauvegardé localement !
-                  </p>
-                  <A href="/link">
-                    Ajouter un compte Pronote
-                  </A>
-                </div>
+                <A href="/link" class="font-medium px-6 py-4 shadow-md rounded-lg dark:bg-brand-primary dark:bg-opacity-20 dark:border-2 dark:border-brand-primary bg-brand-light bg-opacity-20 border-2 border-brand-light">
+                    Cliquez pour associer un compte Pronote!
+                </A>
               }
             >
-              <div class="flex flex-col gap-4">
-                <For each={sessions}>
-                  {session => (
-                    <A href={`/app/${session.slug}`}>
-                      <div
-                        class="
+              <For each={sessions}>
+                {session => (
+                  <A href={`/app/${session.slug}`}>
+                    <div
+                      class="
                           bg-brand-white rounded-xl text-brand-primary
                           p-4 cursor-pointer hover:bg-opacity-80 transition-colors
                           hover:shadow-sm
                         "
-                      >
-                        <h2 class="font-semibold">
-                          {session.user_name}
-                        </h2>
-                        <p class="text-opacity-60">
-                          {session.instance_name}
-                        </p>
-                      </div>
-                    </A>
-                  )}
-                </For>
-                <A href="/link">
+                    >
+                      <h2 class="font-semibold">
+                        {session.user_name}
+                      </h2>
+                      <p class="text-opacity-60">
+                        {session.instance_name}
+                      </p>
+                    </div>
+                  </A>
+                )}
+              </For>
+              <A href="/link">
                   Ajouter un autre compte Pronote
-                </A>
-              </div>
+              </A>
             </Show>
           )}
         </Show>
       </section>
 
-      <footer class="w-full fixed bottom-0 flex flex-col items-center justify-center h-16">
+      <footer class="w-full flex flex-col items-center justify-center">
         <a
-          class="font-medium text-brand-light dark:text-brand-white hover:text-opacity-60 transition-colors"
+          class="font-medium text-brand-light dark:text-brand-white dark:text-opacity-40 hover:text-opacity-60 transition-colors"
           href="https://github.com/Vexcited/pornote"
           rel="noopener noreferrer"
           target="_blank"
