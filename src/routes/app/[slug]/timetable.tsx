c8749b8684146edece902c9f38821a4b73d6412c
Mikkel RINGAUD <mikkel@milescode.dev>
2022-11-14 00:27:01 +0100

    styles: proposal for desktop timetable

--- src/routes/app/[slug]/timetable.tsx
+++ src/routes/app/[slug]/timetable.tsx
@@ -2,19 +2,42 @@ import type { Component } from "solid-js";
 
 import {
   type TimetableLesson,
+  type TimetableBreak,
+
+  getCurrentWeekNumber,
+  getLabelOfPosition,
+  getTimeFormattedDiff,
 
   callUserTimetableAPI,
-  getCurrentWeekNumber,
-  parseTimetableLessons
+  parseTimetableLessons,
+  getDayNameFromDayNumber
 } from "@/utils/client";
 
 import app from "@/stores/app";
+import dayjs from "dayjs";
 
 const AppTimetable: Component = () => {
-  onMount(() => console.groupCollapsed("timetable"));
-  onCleanup(() => console.groupEnd());
+  const [windowHeight, setWindowHeight] = createSignal(window.innerHeight);
+  const windowResizeHandler = () => setWindowHeight(window.innerHeight);
+
+  onMount(() => {
+    console.groupCollapsed("timetable");
+    window.addEventListener("resize", windowResizeHandler);
+  });
+
+  onCleanup(() => {
+    console.groupEnd();
+    window.removeEventListener("resize", windowResizeHandler);
+  });
 
   const [weekNumber, setWeekNumber] = createSignal(getCurrentWeekNumber());
+  const [dayNumber, setDayNumber] = createSignal(dayjs().day());
+
+  const sanitizeDayNumber = (number: number) => {
+    if (number < 0) return 6;
+    if (number > 6) return 0;
+    return number;
+  };
 
   const endpoint = () => app.current_user.endpoints?.[`/user/timetable/${weekNumber()}`];
 
@@ -27,6 +50,8 @@ const AppTimetable: Component = () => {
   }));
 
   const timetable = () => endpoint() ? parseTimetableLessons(endpoint()!.donnees.ListeCours) : null;
+
+  const item_height = () => windowHeight() / ((app.current_user.endpoints?.["/login/informations"].donnees.General.ListeHeures.V.length ?? 0) - 1);
 
   return (
     <div>
@@ -41,38 +66,84 @@ const AppTimetable: Component = () => {
         }
       >
         {timetable_entries => (
-          <div>
-            <div class="flex">
+          <div class="pt-2 pb-8 w-full flex flex-col justify-center items-center">
+            <div class="flex gap-6">
               <For each={timetable_entries}>
-                {days => (
-                  <div class="flex flex-col">
-                    <Index each={days}>
-                      {(lesson_raw, pos) => (
-                        <Switch>
-                          <Match when={lesson_raw() === null}>
-                            <p>Trou à {pos}</p>
-                          </Match>
-                          <Match keyed when={typeof lesson_raw() !== "number" && lesson_raw() as TimetableLesson}>
-                            { lesson => (
-                              <div class="mb-2">
-                                à {lesson.date.format("HH[h]mm")}
+                {(lessons, day_index) => (
+                  <div
+                    classList={{
+                      "hidden md:flex": day_index() !== dayNumber(),
+                      "md:hidden": lessons.length === 0
+                    }}
+                    class="flex flex-col pb-6 bg-brand-primary rounded-md"
+                  >
+                    <div class="flex justify-center items-center gap-4 py-4">
+                      <button
+                        onClick={() => setDayNumber(prev => sanitizeDayNumber(--prev))}
+                        class="md:hidden flex px-2 py-1 rounded-full"
+                      >
+                        <IconMdiArrowLeft />
+                      </button>
+                      <h3 class="font-medium text-xl text-brand-light">{getDayNameFromDayNumber(day_index())}</h3>
+                      <button
+                        onClick={() => setDayNumber(prev => sanitizeDayNumber(++prev))}
+                        class="md:hidden flex px-2 py-1 rounded-full"
+                      >
+                        <IconMdiArrowRight />
+                      </button>
+                    </div>
 
-                                <Show when={lesson.name}>
-                                  <p>{lesson.name}</p>
-                                </Show>
-                                <Show when={lesson.teacher}>
-                                  <p>{lesson.teacher}</p>
-                                </Show>
-                                <Show when={lesson.room}>
-                                  <p>{lesson.room}</p>
-                                </Show>
-
-                              </div>
-                            )}
-                          </Match>
-                        </Switch>
-                      )}
-                    </Index>
+                    <Show when={lessons.length > 0}
+                      fallback={<p>Aucun cours.</p>}
+                    >
+                      <Index each={lessons}>
+                        {(lesson_raw, lesson_index) => (
+                          <div class="relative w-full">
+                            <Switch>
+                              <Match keyed when={lesson_raw().type === "break" && lesson_raw() as TimetableBreak}>
+                                {lesson => (
+                                  <div
+                                    class="bg-brand-white bg-opacity-20 border-2 border-brand-white flex items-center justify-center"
+                                    style={{
+                                      "height": item_height() * (lesson.to - lesson.from) + "px"
+                                    }}
+                                  >
+                                    <p class="text-brand-white text-sm text-center">Pause de {getLabelOfPosition(lesson.from)} à {getLabelOfPosition(lesson.to)} ({
+                                      getTimeFormattedDiff(
+                                        { value: getLabelOfPosition(lesson.from) as string, format: "HH[h]mm" },
+                                        { value: getLabelOfPosition(lesson.to) as string, format: "HH[h]mm" },
+                                        "HH[h]mm"
+                                      )
+                                    })
+                                    </p>
+                                  </div>
+                                )}
+                              </Match>
+                              <Match keyed when={lesson_raw().type === "lesson" && lesson_raw() as TimetableLesson}>
+                                {lesson => (
+                                  <>
+                                    <span class="absolute -top-3.5 bg-brand-light px-4 py-0.5 text-sm rounded w-max left-0 -right-2 ml-auto">{getLabelOfPosition(lesson.position)}</span>
+                                    <div
+                                      style={{
+                                        "border-color": lesson.color,
+                                        "height": item_height() * lesson.duration + "px"
+                                      }}
+                                      class="border-l-4 border-l-brand-primary bg-brand-white px-4 py-2.5"
+                                    >
+                                      <h5 class="font-medium text-lg md:text-base">{lesson.name}</h5>
+                                      <span class="block text-sm">{lesson.room} - {lesson.teacher}</span>
+                                    </div>
+                                    <Show when={lesson_index === lessons.length - 1}>
+                                      <span class="absolute -bottom-3.5 bg-brand-light px-4 py-0.5 text-sm rounded w-max left-0 -right-2 ml-auto">{getLabelOfPosition(lesson.position + lesson.duration)}</span>
+                                    </Show>
+                                  </>
+                                )}
+                              </Match>
+                            </Switch>
+                          </div>
+                        )}
+                      </Index>
+                    </Show>
                   </div>
                 )}
               </For>
