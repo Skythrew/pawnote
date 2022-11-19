4be4945bdb93a6430bd0abcbae8b1b8591fbe6e5
Mikkel RINGAUD <mikkel@milescode.dev>
2022-11-19 19:00:48 +0100

    styles: improve link page state and dark mode

--- src/routes/link.tsx
+++ src/routes/link.tsx
@@ -1,5 +1,5 @@
 import type { Component, JSX } from "solid-js";
-import { ApiGeolocation, ApiInstance } from "@/types/api";
+import type { ApiGeolocation, ApiInstance } from "@/types/api";
 
 import { A } from "solid-start";
 
@@ -32,12 +32,20 @@ import SessionFromScratchModal from "@/components/modals/SessionFromScratch";
 const LinkPronoteAccount: Component = () => {
   const [state, setState] = createStore<{
     info_dialog_open: boolean;
+    show_geolocation_data: boolean;
+
+    loading_instance: boolean;
+    loading_geolocation: boolean;
 
     pronote_url: string;
     instance_data: ApiInstance["response"] | null;
     geolocation_data: ApiGeolocation["response"] | null;
   }>({
     info_dialog_open: false,
+    show_geolocation_data: false,
+
+    loading_instance: false,
+    loading_geolocation: false,
 
     pronote_url: "",
     instance_data: null,
@@ -54,9 +62,14 @@ const LinkPronoteAccount: Component = () => {
   );
 
   const handleGeolocation = async () => {
-    setState("geolocation_data", null);
+    if (state.geolocation_data) {
+      setState("show_geolocation_data", true);
+      return;
+    }
 
     try {
+      setState("loading_geolocation", true);
+
       const {
         coords: {
           latitude, longitude
@@ -64,6 +77,9 @@ const LinkPronoteAccount: Component = () => {
       } = await getGeolocationPosition();
 
       const data = await callAPI<ApiGeolocation>("/geolocation", () => ({ latitude, longitude }));
+
+      setState("loading_geolocation", false);
+
       if (data.length <= 0) {
         alert("Aucune instance Pronote proche de votre location n'a été trouvée.");
         return;
@@ -72,10 +88,14 @@ const LinkPronoteAccount: Component = () => {
       setState({
         geolocation_data: data,
         // Automatically select the first URL.
-        pronote_url: data[0].url
+        pronote_url: data[0].url,
+
+        show_geolocation_data: true
       });
     }
     catch (err) {
+      setState("loading_geolocation", false);
+
       if (err instanceof ApiError) {
         console.error(err.message);
         alert("Une erreur est survenue. Vérifiez la console.");
@@ -92,22 +112,30 @@ const LinkPronoteAccount: Component = () => {
     evt.preventDefault();
 
     try {
+      setState("loading_instance", true);
+
       const response = await callAPI<ApiInstance>("/instance", () => ({
         pronote_url: state.pronote_url
       }));
 
       batch(() =>  {
-        setState("instance_data", response);
+        setState({
+          loading_instance: false,
+          instance_data: response
+        });
+
         app.setModal("needs_scratch_session", true);
       });
     }
     catch (err) {
+      setState("loading_instance", false);
       console.error(err);
+      alert("Une erreur est survenue pendant la connexion à l'instance choisie.");
     }
   };
 
   return (
-    <div class="min-h-screen bg-brand-primary">
+    <div class="min-h-screen bg-brand-primary dark:bg-brand-dark">
       <header class="p-4 pb-8">
         <A class="flex w-max text-xl p-2 text-brand-light" href="/">
           <IconMdiArrowLeft />
@@ -121,12 +149,11 @@ const LinkPronoteAccount: Component = () => {
             Connexion à Pronote
           </h1>
           <button
-            class="text-brand-light border-b-2 border-brand-light"
+            class="text-brand-light border-b-2 border-brand-light dark:text-brand-primary dark:border-brand-primary"
             onClick={() => setState("info_dialog_open", true)}
           >
             Comment faire ?
           </button>
-
         </div>
 
         <Portal>
@@ -156,7 +183,7 @@ const LinkPronoteAccount: Component = () => {
                   class="inline-block h-screen align-middle"
                   aria-hidden="true"
                 >
-              &#8203;
+                  &#8203;
                 </span>
                 <TransitionChild
                   class="transform"
@@ -167,15 +194,15 @@ const LinkPronoteAccount: Component = () => {
                   leaveFrom="opacity-100 scale-100"
                   leaveTo="opacity-0 scale-95"
                 >
-                  <DialogPanel class="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle border-2 border-brand-primary bg-brand-white shadow-xl rounded-md">
+                  <DialogPanel class="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle border-2 border-brand-primary bg-brand-white dark:bg-brand-dark shadow-xl rounded-md">
                     <DialogTitle
                       as="h3"
                       class="text-lg font-semibold text-brand-primary text-center"
                     >
                       Comment se connecter?
                     </DialogTitle>
-                    <div class="mt-2 flex flex-col gap-2">
-                      <p class="text-brand-dark font-medium">Pour effectuer la première étape de votre connexion, vous allez devoir choisir une instance Pronote.</p>
+                    <div class="mt-2 flex flex-col gap-2 text-brand-dark dark:text-brand-white">
+                      <p class="font-medium">Pour effectuer la première étape de votre connexion, vous allez devoir choisir une instance Pronote.</p>
                       <p>Pour cela, vous devez coller <b>l'URL de l'instance Pronote</b> dans la boite de texte.</p>
                       <p>Autrement, vous pouvez cliquer sur le bouton de <b>Géolocalisation</b> pour trouver les instances Pronote proche de votre localisation et directement les utiliser.</p>
                     </div>
@@ -198,17 +225,16 @@ const LinkPronoteAccount: Component = () => {
 
         <form class="space-y-4" onSubmit={processInformations}>
           <div class="flex">
-            <Show when={state.geolocation_data !== null}>
+            <Show when={state.show_geolocation_data}>
               <Listbox defaultOpen value={state.pronote_url} onSelectChange={instanceSelectChange}>
-                <div class="relative w-full">
-                  <ListboxButton type="button" class="relative w-full py-2 pl-3 pr-10 text-left bg-brand-white rounded-lg rounded-r-none cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-brand-primary focus-visible:ring-offset-2 focus-visible:border-brand-primary sm:text-sm">
-                    <span class="block truncate">{checkMatchPronoteUrl()?.name || state.pronote_url}</span>
+                <div class="relative w-full min-w-0">
+                  <ListboxButton type="button" class="relative w-full h-full py-2 pl-4 pr-10 text-left bg-brand-white rounded-lg rounded-r-none cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-brand-primary focus-visible:ring-offset-2 focus-visible:border-brand-primary sm:text-sm">
+                    <span class="block truncate text-sm">{checkMatchPronoteUrl()?.name || state.pronote_url}</span>
                     <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                      {/* <SelectorIcon
-                        class="w-5 h-5 text-gray-400"
+                      <IconMdiChevronRight
+                        class="w-5 h-5"
                         aria-hidden="true"
-                      /> */}
-                      {">"}
+                      />
                     </span>
                   </ListboxButton>
                   <HeadlessDisclosureChild>
@@ -222,7 +248,7 @@ const LinkPronoteAccount: Component = () => {
                         leaveFrom="opacity-100"
                         leaveTo="opacity-0"
                       >
-                        <ListboxOptions class="list-none p-0 absolute py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                        <ListboxOptions class="list-none p-0 absolute py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg h-[35vh] ring-1 ring-black ring-opacity-5 focus:outline-none text-sm md:text-base">
                           <For each={state.geolocation_data}>
                             {instance => (
                               <ListboxOption class="focus:outline-none group" value={instance.url}>
@@ -247,8 +273,7 @@ const LinkPronoteAccount: Component = () => {
                                       <span
                                         class="text-brand-white absolute inset-y-0 left-0 flex items-center pl-3"
                                       >
-                                        OK
-                                        {/* <CheckIcon class="w-5 h-5" aria-hidden="true" /> */}
+                                        <IconMdiCheck class="w-5 h-5" aria-hidden="true" />
                                       </span>
                                     ) : null}
                                   </div>
@@ -262,26 +287,30 @@ const LinkPronoteAccount: Component = () => {
                   </HeadlessDisclosureChild>
                 </div>
               </Listbox>
-              <button class="bg-brand-light p-2 px-4 rounded-lg rounded-l-none" type="button" onClick={() => setState("geolocation_data", null)}>
-                Editer
+              <button class="bg-brand-light p-2 px-4 rounded-lg flex rounded-l-none text-brand-dark dark:bg-brand-primary dark:text-brand-light" type="button" onClick={() => setState("show_geolocation_data", false)}>
+                <IconMdiPencil />
               </button>
             </Show>
 
-            <Show when={!state.geolocation_data}>
-              <input class="w-full rounded-lg rounded-r-none outline-none px-2 text-sm"
+            <Show when={!state.show_geolocation_data}>
+              <input class="w-full rounded-lg rounded-r-none outline-none px-4 text-sm"
                 type="url"
+                placeholder="URL Pronote"
                 value={state.pronote_url}
                 onChange={event => setState("pronote_url", event.currentTarget.value)}
               />
 
-              <button class="bg-brand-light py-2 px-4 rounded-lg flex items-center justify-center rounded-l-none text-brand-dark" type="button" onClick={handleGeolocation}>
-                <IconMdiMapMarkerRadius />
+              <button disabled={state.loading_geolocation} class="bg-brand-light py-2 px-4 rounded-lg flex rounded-l-none text-brand-dark dark:bg-brand-primary dark:text-brand-light disabled:opacity-60" type="button" onClick={handleGeolocation}>
+                {state.loading_geolocation
+                  ? <IconMdiDotsHorizontal />
+                  : <IconMdiMapMarkerRadius />
+                }
               </button>
             </Show>
           </div>
 
-          <button disabled={!state.pronote_url} class="disabled:opacity-40 bg-brand-light px-4 py-2 rounded-lg w-full" type="submit">
-            Etablir une connexion
+          <button disabled={!state.pronote_url || state.loading_instance} class="disabled:opacity-40 bg-brand-light px-4 py-2 rounded-lg w-full dark:bg-brand-primary dark:text-brand-light" type="submit">
+            {state.loading_instance ? "Connexion..." : "Etablir une connexion"}
           </button>
         </form>
 
@@ -298,3 +327,4 @@ const LinkPronoteAccount: Component = () => {
 };
 
 export default LinkPronoteAccount;
+
