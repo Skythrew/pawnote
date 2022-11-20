beafa9323df78c4efda01ae580d5b6e3dc1e9716
Mikkel RINGAUD <mikkel@milescode.dev>
2022-11-20 21:53:55 +0100

    styles: homeworks page

--- src/routes/app/[slug]/homeworks.tsx
+++ src/routes/app/[slug]/homeworks.tsx
@@ -31,7 +31,7 @@ const AppHomeworks: Component = () => {
 
   return (
     <div class="flex flex-col items-center gap-2">
-      <h2>Devoirs de la semaine {weekNumber()} !</h2>
+      <h2>Devoirs de la semaine {weekNumber()}</h2>
       <div class="flex gap-2 justify-center items-center">
         <button
           class="px-4 py-1 rounded-full bg-brand-light flex"
@@ -55,16 +55,20 @@ const AppHomeworks: Component = () => {
         }
       >
         {homeworks => (
-          <div class="mt-4 flex gap-2">
+          <div class="my-8 px-6 flex flex-col gap-2">
             <For each={Object.keys(homeworks).map(Number)}>
               {day_index => (
                 <Show when={homeworks[day_index].length > 0}>
-                  <div class="p-2 rounded-md bg-brand-primary">
-                    <h2 class="text-md font-medium">{getDayNameFromDayNumber(day_index)}</h2>
+                  <div class="px-2 py-5 rounded-md bg-brand-primary max-w-md w-full flex flex-col gap-2 relative">
+                    <h2 class="absolute bg-brand-light -left-4 -top-5 text-md font-medium text-brand-primary pl-6 right-4 py-1 rounded-full shadow">{getDayNameFromDayNumber(day_index)}</h2>
                     <For each={homeworks[day_index]}>
                       {homework => (
-                        <div class="p-2 rounded bg-brand-white">
-                          <h3 class="font-medium">{homework.subject_name}</h3>
+                        <div class="p-2 rounded bg-brand-white flex-col">
+                          <div class="flex justify-between items-center">
+                            <h3 class="font-medium">{homework.subject_name}</h3>
+                            <input type="checkbox" checked={homework.done} />
+                          </div>
+
                           <div innerHTML={homework.description} />
                         </div>
                       )}
