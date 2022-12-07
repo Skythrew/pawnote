d522508891ec2aaadfb85b7d152f564f3360e136
Mikkel RINGAUD <mikkel@milescode.dev>
2022-12-07 23:03:17 +0100

    styles(homework): add desktop view and new design

--- src/routes/app/[slug]/homeworks.tsx
+++ src/routes/app/[slug]/homeworks.tsx
@@ -5,14 +5,13 @@ import {
   getDayNameFromDayNumber,
 
   callUserHomeworksAPI,
-  callUserTimetableAPI,
   callUserHomeworkDoneAPI,
 
   parseHomeworks,
   createExternalFileURL
 } from "@/utils/client";
 
-import app from "@/stores/app";
+import app, { AppStateCode } from "@/stores/app";
 
 const AppHomeworks: Component = () => {
   onMount(() => console.groupCollapsed("homeworks"));
@@ -23,12 +22,11 @@ const AppHomeworks: Component = () => {
   const endpoint = () => app.current_user.endpoints?.[`/user/homeworks/${weekNumber()}`];
 
   /** Renew the homeworks when needed. */
-  createEffect(on(weekNumber, async (week) => {
-    console.groupCollapsed(`Week ${week}`);
+  createEffect(on([weekNumber, () => app.current_user.session], async () => {
+    console.groupCollapsed(`Week ${weekNumber()}`);
     onCleanup(() => console.groupEnd());
 
-    await callUserHomeworksAPI(week);
-    await callUserTimetableAPI(week);
+    await callUserHomeworksAPI(weekNumber());
   }));
 
   const homeworks = createMemo(() => endpoint()
@@ -66,83 +64,99 @@ const AppHomeworks: Component = () => {
             </div>
           }
         >
-          <div class="my-8 md:px-6 md:max-w-md w-full flex flex-col">
-            <For each={Object.keys(homeworks()!).map(Number)}
-              fallback={
-                <div class="mx-auto w-max border-2 border-brand-primary px-4 py-2 rounded-md">
-                  <p class="text-center">Aucun devoirs cette semaine !</p>
-                </div>
-              }
-            >
-              {day_index => (
-                <Show when={homeworks()![day_index].length > 0}>
-                  <div class="rounded-md flex flex-col relative">
-                    <h2 class="dark:(bg-brand-primary text-brand-light) bg-brand-light md:(absolute -left-4 right-4 rounded-full -top-5) text-md font-medium text-brand-primary pl-6 py-1">{getDayNameFromDayNumber(day_index)}</h2>
-                    <For each={homeworks()![day_index]}>
-                      {(homework, homework_index) => (
-                        <div style={{ "border-color": homework.subject_color }}
-                          class="relative py-3 pl-4 mx-4 bg-brand-white dark:(bg-brand-dark text-brand-white) border-l-4 flex-col gap-2">
-                          <div class="flex justify-between items-center mb-2">
-                            <div>
-                              <h3 class="text-md font-medium">{homework.subject_name}</h3>
-                              <span>Pour {app.current_user.endpoints?.[`/user/timetable/${weekNumber()}`]?.donnees.ListeCours.find(
-                                item => item.N === homework.subject_timetable_id
-                              )?.DateDuCours.V ?? "EDT non récupéré"}
-                              </span>
-                            </div>
-                            <label class="flex text-xs gap-2 rounded-full border px-3 py-1 items-center"
-                              classList={{
-                                "border-brand-primary bg-brand-light text-brand-primary dark:(bg-dark-200 text-brand-light border-transparent)": homework.done,
-                                "border-brand-dark dark:border-brand-white": !homework.done
-                              }}
-                            >
-                              {homework.done
-                                ? <>Fait <IconMdiCheck /></>
-                                : <>Non Fait <IconMdiClose /></>
-                              }
-                              <input
-                                hidden
-                                type="checkbox"
-                                checked={homework.done}
-                                onChange={(event) => {
-                                  callUserHomeworkDoneAPI({
-                                    homework_id: homework.id,
-                                    week_number: weekNumber(),
-                                    done: event.currentTarget.checked
-                                  });
-                                }}
-                              />
-                            </label>
-                          </div>
+          <div class="flex my-8 md:px-4 justify-center">
+            <div class="hidden md:block max-w-72 w-full">
+              <div class="sticky"
+                classList={{
+                  // Header is `h-18` and app state banner is `h-8`.
+                  // We add `+2` padding on `md`.
+                  "top-18 md:top-20": app.current_state.code === AppStateCode.Idle,
+                  "top-26 md:top-28": app.current_state.code !== AppStateCode.Idle
+                }}
+              >
+                <h4 class="font-medium w-max text-md bg-brand-light text-brand-primary px-8 py-2 rounded-full dark:(bg-dark-200 text-brand-light)">Filtres</h4>
+              </div>
+            </div>
 
-                          <div class="text-sm break-words" innerHTML={homework.description} />
-
-                          <Show when={homework.attachments.length > 0}>
-                            <div class="mt-2 flex flex-wrap gap-2">
-                              <For each={homework.attachments}>
-                                {attachment => (
-                                  <a
-                                    class="text-xs px-2 py-1 bg-brand-light rounded-md dark:text-brand-dark"
-                                    href={createExternalFileURL(attachment)}
-                                    target="_blank"
-                                  >
-                                    {attachment.name}
-                                  </a>
-                                )}
-                              </For>
-                            </div>
-                          </Show>
-
-                          <Show when={homeworks()![day_index].length - 1 !== homework_index()}>
-                            <span class="z-10 h-[2px] absolute -bottom-[1px] left-2 right-0 bg-brand-dark opacity-20 dark:(bg-dark-200 opacity-100)" />
-                          </Show>
-                        </div>
-                      )}
-                    </For>
+            <div class="w-full md:max-w-4xl flex flex-col">
+              <For each={Object.keys(homeworks()!).map(Number)}
+                fallback={
+                  <div class="mx-auto w-max border-2 border-brand-primary px-4 py-2 rounded-md">
+                    <p class="text-center">Aucun devoirs cette semaine !</p>
                   </div>
-                </Show>
-              )}
-            </For>
+                }
+              >
+                {day_index => (
+                  <Show when={homeworks()![day_index].length > 0}>
+                    <div class="rounded-md flex flex-col relative">
+                      <h2 class="z-20 sticky shadow-lg shadow-brand-white dark:shadow-brand-dark md:mb-4 dark:(bg-dark-200 text-brand-light) md:rounded-full py-2 bg-brand-light text-md font-medium text-brand-primary pl-6"
+                        classList={{
+                          // Header is `h-18` and app state banner is `h-8`.
+                          // We add `+2` padding on `md`.
+                          "top-18 md:top-20": app.current_state.code === AppStateCode.Idle,
+                          "top-26 md:top-28": app.current_state.code !== AppStateCode.Idle
+                        }}
+                      >{getDayNameFromDayNumber(day_index)}</h2>
+                      <For each={homeworks()![day_index]}>
+                        {(homework, homework_index) => (
+                          <div style={{ "border-color": homework.subject_color }}
+                            class="relative py-3 px-4 md:(ml-8 mr-4 rounded-lg mb-2) bg-brand-dark/2 dark:(bg-dark-300/25 text-brand-white) dark:hover:bg-dark-300/50 transition-colors border-l-4 flex flex-col gap-2">
+                            <div class="flex justify-between items-center">
+                              <h3 class="text-md font-medium dark:font-semibold">{homework.subject_name}</h3>
+                              <label class="cursor-pointer flex text-xs gap-2 rounded-full border px-3 py-1 items-center"
+                                classList={{
+                                  "border-brand-primary bg-brand-light text-brand-primary dark:(bg-dark-200 text-brand-light border-transparent)": homework.done,
+                                  "border-brand-dark dark:border-brand-white": !homework.done
+                                }}
+                              >
+                                {homework.done
+                                  ? <>Fait <IconMdiCheck /></>
+                                  : <>Non Fait <IconMdiClose /></>
+                                }
+                                <input
+                                  hidden
+                                  type="checkbox"
+                                  checked={homework.done}
+                                  onChange={(event) => {
+                                    callUserHomeworkDoneAPI({
+                                      homework_id: homework.id,
+                                      week_number: weekNumber(),
+                                      done: event.currentTarget.checked
+                                    });
+                                  }}
+                                />
+                              </label>
+                            </div>
+
+                            <div class="text-sm break-words" innerHTML={homework.description} />
+
+                            <Show when={homework.attachments.length > 0}>
+                              <div class="mt-2 flex flex-wrap gap-2">
+                                <For each={homework.attachments}>
+                                  {attachment => (
+                                    <a
+                                      class="text-xs px-2 py-1 bg-brand-light text-brand-dark rounded-md dark:(border border-brand-light bg-dark-300 text-brand-light)"
+                                      href={createExternalFileURL(attachment)}
+                                      target="_blank"
+                                    >
+                                      {attachment.name}
+                                    </a>
+                                  )}
+                                </For>
+                              </div>
+                            </Show>
+
+                            <Show when={homeworks()![day_index].length - 1 !== homework_index()}>
+                              <span class="md:hidden z-10 h-[2px] absolute -bottom-[1px] left-0 right-0 bg-brand-dark opacity-5 dark:(bg-dark-200 opacity-100)" />
+                            </Show>
+                          </div>
+                        )}
+                      </For>
+                    </div>
+                  </Show>
+                )}
+              </For>
+            </div>
           </div>
         </Show>
       </div>
