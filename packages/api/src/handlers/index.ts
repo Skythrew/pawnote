bc905779839ef87a578f98ea0ffa6f2cd5fb287c
Mikkel RINGAUD <mikkel@milescode.dev>
2022-12-23 15:31:34 +0000

    feat(api): named export for handlers and export internal typings

---
+++ packages/api/src/handlers/index.ts
@@ -0,0 +1,2 @@
+export { default as instance } from "@/handlers/instance";
+export { default as geolocation } from "@/handlers/geolocation";
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,2 +1,12 @@
-export { instance } from "@/handlers/instance";
+export * as handlers from "@/handlers";
+
+// Handlers typings.
 export type { PronoteApiInstance, ApiInstance } from "@/handlers/instance/types";
+export type { PronoteApiGeolocation, ApiGeolocation } from "@/handlers/geolocation/types";
+
+// Internals typings.
+export type { HttpCallFunction, Response, ResponseError, ResponseSuccess } from "@/types/internals";
+
+// Utilities that can be used outside.
+export { PRONOTE_ACCOUNT_TYPES } from "@/utils/constants";
+export { cleanPronoteUrl } from "@/utils/globals";
