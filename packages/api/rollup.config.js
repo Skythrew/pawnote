7c6baf1d71851d9d0119a6ea64fede11a262d321
Mikkel RINGAUD <mikkel@milescode.dev>
2022-12-23 20:25:34 +0000

    chore(api): add `commonjs` rollup plugin

--- packages/api/rollup.config.js
+++ packages/api/rollup.config.js
@@ -3,11 +3,12 @@ import esbuild from 'rollup-plugin-esbuild'
 import tsConfigPaths from "rollup-plugin-tsconfig-paths"
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default [
   {
     input: "src/index.ts",
-    plugins: [resolve(), typescript(), esbuild()],
+    plugins: [resolve(), commonjs(), typescript(), esbuild()],
     output: {
       dir: "dist",
       format: 'es',
