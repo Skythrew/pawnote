71dd8391afe6d6772516e69228bc5e484dddb496
Mikkel RINGAUD <mikkel@milescode.dev>
2023-01-21 19:47:35 +0100

    fix(api): rollup configuration

--- packages/api/rollup.config.js
+++ packages/api/rollup.config.js
@@ -2,18 +2,17 @@ import dts from 'rollup-plugin-dts'
 import esbuild from 'rollup-plugin-esbuild'
 import tsConfigPaths from "rollup-plugin-tsconfig-paths"
 import resolve from '@rollup/plugin-node-resolve'
-import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default [
   {
     input: "src/index.ts",
-    plugins: [resolve(), commonjs(), typescript(), esbuild()],
+    plugins: [resolve(), tsConfigPaths(), commonjs(), esbuild({ minify: true })],
     output: {
       dir: "dist",
-      format: 'es',
-      sourcemap: true,
-    },
+      format: "es",
+      sourcemap: true
+    }
   },
   {
     input: "src/index.ts",
