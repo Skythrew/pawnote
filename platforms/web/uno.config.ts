7b69eac25b098e656610d5b4887272a18989f9da
Mikkel RINGAUD <mikkel@milescode.dev>
2023-05-02 01:09:50 +0200

    styles: add animation keyframes for modalss

--- platforms/web/uno.config.ts
+++ platforms/web/uno.config.ts
@@ -1,13 +1,24 @@
+import type { Theme } from "@unocss/preset-uno";
+import type { Preset } from "unocss";
+
 import { defineConfig } from "unocss";
 
+// Presets.
 import presetUno from "@unocss/preset-uno";
+import { presetKobalte } from "unocss-preset-primitives";
+
+// Transformers.
 import transformerVariantGroup from "@unocss/transformer-variant-group";
 
+// Themes.
 import { extendCatppuccin } from "unocss-catppuccin-colours";
 
 export default defineConfig({
   presets: [
-    presetUno()
+    presetUno(),
+    presetKobalte({
+      prefix: "ui"
+    }) as Preset<Theme>
   ],
 
   transformers: [
@@ -19,6 +30,13 @@ export default defineConfig({
 
     fontFamily: {
       sans: "Comfortaa"
+    },
+
+    animation: {
+      keyframes: {
+        "scale-in": "{from{opacity:0;transform:scale(0.96)}to{opacity:1;transform:scale(1)}}",
+        "scale-out": "{from{opacity:1;transform:scale(&)}to{opacity:0;transform:scale(0.96)}}"
+      }
     }
   }
 });
