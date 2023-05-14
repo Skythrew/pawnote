0d654d4bcc3b49758fc687f17a59bd01303cbe38
Mikkel RINGAUD <mikkel@milescode.dev>
2023-05-15 00:21:26 +0200

    chore(api): reexport all pronote api types

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -6,6 +6,7 @@ export type { PronoteApiGeolocation, ApiGeolocation } from "@/handlers/geolocati
 
 // Internals typings.
 export type { HttpCallFunction, Response, ResponseError, ResponseSuccess } from "@/types/internals";
+export * from "@/types/pronote_api";
 
 // Utilities that can be used outside.
 export { PRONOTE_ACCOUNT_TYPES } from "@/utils/constants";
