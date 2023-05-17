01a984741a6075620f098bae072934915b5425e2
Mikkel RINGAUD <mikkel@milescode.dev>
2023-05-17 23:44:44 +0200

    chore(web): remove useless validation

--- platforms/web/src/routes/api/geolocation.ts
+++ platforms/web/src/routes/api/geolocation.ts
@@ -1,15 +1,7 @@
-import { type ApiGeolocation, ResponseErrorCode, handlers } from "@pawnote/api";
-
+import { type ApiGeolocation, handlers } from "@pawnote/api";
 import { handleServerRequest } from "@/utils/server";
-import { objectHasProperty } from "@/utils/globals";
 
 export const POST = handleServerRequest<ApiGeolocation>(async (req, res) => {
-  if (!objectHasProperty(req.body, "latitude") || !objectHasProperty(req.body, "longitude"))
-    return res.error({
-      code: ResponseErrorCode.MissingParameters,
-      debug: { received_body: req.body }
-    }, { status: 400 });
-
-  const api = await handlers.geolocation(req.fetcher, req.body);
+  const api = await handlers.geolocation(req.fetcher, req.body, {}, req.user_agent);
   return res.from(api);
 });
--- platforms/web/src/routes/api/instance.ts
+++ platforms/web/src/routes/api/instance.ts
@@ -1,15 +1,7 @@
-import { type ApiInstance, ResponseErrorCode, handlers } from "@pawnote/api";
-
+import { type ApiInstance, handlers } from "@pawnote/api";
 import { handleServerRequest } from "@/utils/server";
-import { objectHasProperty } from "@/utils/globals";
 
 export const POST = handleServerRequest<ApiInstance>(async (req, res) => {
-  if (!objectHasProperty(req.body, "pronote_url"))
-    return res.error({
-      code: ResponseErrorCode.MissingParameters,
-      debug: { received_body: req.body }
-    }, { status: 400 });
-
-  const api = await handlers.instance(req.fetcher, req.body);
+  const api = await handlers.instance(req.fetcher, req.body, {}, req.user_agent);
   return res.from(api);
 });
