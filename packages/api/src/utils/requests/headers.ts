38c268e9083d9c54983bc40df2060f113149be61
Mikkel RINGAUD <mikkel@milescode.dev>
2023-05-28 10:34:49 +0200

    refactor: `cookies` -> `headers`

--- packages/api/src/utils/requests/headers.ts
+++ packages/api/src/utils/requests/headers.ts
@@ -9,3 +9,7 @@ export const retrieveResponseCookies = (headers: Record<string, string> | Header
 
   return cookies;
 };
+
+export const getHeaderFromFetcherResponse = (headers: Record<string, string> | Headers, item: string) => {
+  return headers instanceof Headers ? headers.get(item) : headers[item];
+};
