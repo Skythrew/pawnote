46e7629c7306ae7f4b28e882f0141f75622e28ba
Mikkel RINGAUD <mikkel@milescode.dev>
2023-05-29 21:45:10 +0200

    chore(lint): remove useless rules

--- configs/eslint-config-solid/index.js
+++ configs/eslint-config-solid/index.js
@@ -2,12 +2,21 @@
 // eslint-disable-next-line no-undef
 module.exports = /** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */ ({
   plugins: ["solid"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
   extends: [
     "@pawnote/eslint-config",
     "plugin:solid/typescript"
   ],
   rules: {
     "solid/jsx-no-undef": "off",
-    "solid/no-innerhtml": "off"
+    "solid/no-innerhtml": "off",
+
+    "jsx-quotes": [
+      "error", "prefer-double"
+    ]
   }
 });
--- configs/eslint-config/index.js
+++ configs/eslint-config/index.js
@@ -1,10 +1,6 @@
 // @ts-check
 // eslint-disable-next-line no-undef
 module.exports = /** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */ ({
-  parser: "@typescript-eslint/parser",
-  plugins: [
-    "@typescript-eslint"
-  ],
   extends: "standard-with-typescript",
   rules: {
     "brace-style": [
@@ -14,30 +10,20 @@ module.exports = /** @type {import('@typescript-eslint/utils').TSESLint.Linter.C
       "error", "double"
     ],
     "indent": [
-      "error", 2
+      "error", 2, { "SwitchCase": 1 }
     ],
     "semi": [
       "error", "always"
     ],
-    "eol-last": [
-      "error", "always"
-    ],
-    "eqeqeq": [
-      "error", "always"
-    ],
-    "comma-dangle": [
-      "error", "never"
-    ],
 
     "no-return-await": "error",
-    "no-trailing-spaces": "error",
-    "@typescript-eslint/no-non-null-assertion": "off",
+    "n/no-callback-literal": "off",
+
     "@typescript-eslint/semi": "off",
     "@typescript-eslint/brace-style": "off",
     "@typescript-eslint/quotes": "off",
     "@typescript-eslint/indent": "off",
-
     "@typescript-eslint/naming-convention": "off",
-    "n/no-callback-literal": "off"
+    "@typescript-eslint/no-non-null-assertion": "off"
   }
 });
