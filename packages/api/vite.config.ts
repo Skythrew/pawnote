f9057dbd07fcd0cb8f25aa1225feb11a9be5b9bf
Mikkel RINGAUD <mikkel@milescode.dev>
2023-06-02 23:15:31 +0200

    fix(api): use vite to build to prevent issues on web build

---
+++ packages/api/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import path from "node:path";
+
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [
+    dts({
+      outputDir: "dist",
+      staticImport: true
+    })
+  ],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  },
+
+  build: {
+    lib: {
+      formats: ["es"],
+      entry: path.resolve(__dirname, "src/index.ts"),
+      fileName: "index"
+    }
+  }
+});
